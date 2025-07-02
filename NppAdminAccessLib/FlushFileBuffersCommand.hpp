d064ecaf79ac23a6a8e3ba94a934e09a2861037a
QuentiumYT <quentin.lienhardt@gmail.com>
2025-07-02 21:38:08 +0200

    fix?

--- NppAdminAccessLib/FlushFileBuffersCommand.hpp
+++ NppAdminAccessLib/FlushFileBuffersCommand.hpp
@@ -11,10 +11,10 @@ std::shared_ptr<ICommand> make_flush_file_buffers_command(IWinApiFunctions& wina
 class FlushFileBuffersCommand : public ICommand {
 public:
     explicit FlushFileBuffersCommand(IWinApiFunctions& winapi)
-        : m_winapi(winapi) {}
+        : ICommand(winapi) {} // <-- Call base constructor
 
     bool execute(const std::vector<char>& in_buffer, std::vector<char>& out_buffer) override;
 
 private:
-    IWinApiFunctions& m_winapi;
+    // No need to store m_winapi here, it's already in ICommand
 };
